c7792140108954f5c6f76453484a6a6311ae1ea7
Alonso210 <30377881+Alonso210@users.noreply.github.com>
2017-07-22 15:29:44 -0600

    Codigo
    Código para el Brazo Robotico Simple de 2 ejes

---
+++ Brazo_Robotico/Brazo_Robotico.ino
@@ -0,0 +1,33 @@
+#include <Servo.h>
+const int servo1 = 9;
+const int servo2 = 11;
+const int joyH = A3;
+const int joyV = A1;
+int posX=90;
+
+int servoVal;
+
+Servo myservo1;
+Servo myservo2;
+
+void setup() {
+myservo1.attach(servo1);
+myservo2.attach(servo2);
+Serial.begin(9600);
+myservo2.write(posX);
+
+}
+
+void loop() {
+
+servoVal = analogRead(joyH);
+servoVal = map(servoVal,0,1023,180,0);
+myservo2.write(servoVal);
+
+servoVal = analogRead(joyV);
+servoVal = map(servoVal,0,1023,50,120);
+myservo1.write(servoVal);
+delay(15);
+
+Serial.println(servoVal); 
+}
